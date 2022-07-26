785cbf45c5885c29f6b3ab14c1deca1d781839bf
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-07-26 13:10:49 +0430

    working on navbar section for mobile and desktop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-
+    <div className=''>
+        <Navbar />
+        {/* <Header /> */}
     </div>
+
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import img1 from '../images/bg-pattern-circles.svg'
+
+
+
+function Header() {
+    const bgStyle = {
+        backgroundImage: "url('https://media.geeksforgeeks.org/wp-content/uploads/rk.png')",
+        height: '100vh',
+
+    }
+    return (
+        <div className='header' >
+            Hello World
+        </div>
+    )
+}
+
+export default Header
