c44a062bd9d0dffef320a4a1e93df0da4b997c18
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-07-26 19:36:20 +0430

    Header component created;

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
 
-import img1 from '../images/bg-pattern-circles.svg'
-
-
 
 function Header() {
-    const bgStyle = {
-        backgroundImage: "url('https://media.geeksforgeeks.org/wp-content/uploads/rk.png')",
-        height: '100vh',
-
-    }
     return (
-        <div className='header' >
-            Hello World
+        <div className='header px-4' >
+            <h1 className='flex justify-center mb-10 text-white items-center pt-20 text-center text-4xl font-bold md:text-5xl'>
+                A modern publishing platform
+            </h1>
+            <p className='text-center text-gray-300 text-xl md:text-2xl'>Grow your audience and build your online board</p>
+            <div className='flex justify-center space-x-4 items-center mt-20'>
+                <div className=' px-3 py-2 bg-white text-black font-bold rounded-2xl shadow-lg hover:bg-black hover:text-white'>
+                    <button>Start for Free</button>
+                </div>
+                <div className='text-white px-3 py-2 ring-2 font-bold rounded-2xl shadow-lg hover:bg-white hover:text-black'>
+                    <button>Learn More</button>
+                </div>
+            </div>
         </div>
     )
 }
