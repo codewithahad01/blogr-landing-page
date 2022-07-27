37c22ec287a9ba9c3e4d1345b9aa1378c4be6b14
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-07-27 15:44:06 +0430

    All components added to app.js file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Manfacture from "./components/Manfacture";
 import Navbar from "./components/Navbar";
 import Services from "./components/Services";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Feature />
         <Manfacture />
         <Services />
+        <Footer />
     </div>
 
   );
