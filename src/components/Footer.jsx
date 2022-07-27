99b356995034319a8b19a6a24ebacfefda9ff2c8
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-07-27 15:49:18 +0430

    footer section completed well

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,13 @@ function Footer() {
     return (
         <div className='flex flex-col bg-gray-800 text-white pt-28 pb-10  rounded-t-full  text-center text-lg md:flex-row md:justify-around'>
 
-            <div className='mb-4'>
+            <div className='mb-16'>
                 <p className='text-4xl font-extrabold'>Blogr</p>
             </div>
 
-            <div>
+            <div className='mb-10'>
                 <ul>
-                    <li><a className='text-lg font-bold' href='#home'>Product</a></li>
+                    <li><a className='text-xl font-bold' href='#home'>Product</a></li>
                     <li><a className='md:text-sm hover:underline' href='#home'>Overview</a></li>
                     <li><a className='md:text-sm hover:underline' href='#home'>Pricing</a></li>
                     <li><a className='md:text-sm hover:underline' href='#home'>Marketplace</a></li>
@@ -19,9 +19,9 @@ function Footer() {
                 </ul>
             </div>
             
-            <div>
+            <div className='mb-10'>
                 <ul>
-                    <li><a className='text-lg font-bold' href='#home'>Company</a></li>
+                    <li><a className='text-xl font-bold' href='#home'>Company</a></li>
                     <li><a className='md:text-sm hover:underline' href='#home'>About</a></li>
                     <li><a className='md:text-sm hover:underline' href='#home'>Team</a></li>
                     <li><a className='md:text-sm hover:underline' href='#home'>Blog</a></li>
@@ -31,7 +31,7 @@ function Footer() {
 
             <div>
                 <ul>
-                    <li><a className='text-lg font-bold' href='#home'>Connect</a></li>
+                    <li><a className='text-xl font-bold' href='#home'>Connect</a></li>
                     <li><a className='md:text-sm hover:underline' href='#home'>Connect</a></li>
                     <li><a className='md:text-sm hover:underline' href='#home'>News Letter</a></li>
                     <li><a className='md:text-sm hover:underline' href='#home'>Linkedin</a></li>
