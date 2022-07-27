b7eee145110e34213c4c0ee47aae4f33f163daa3
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-07-27 15:45:21 +0430

    manufucture section completed

--- src/components/Manfacture.jsx
+++ src/components/Manfacture.jsx
@@ -9,9 +9,9 @@ function Manfacture() {
                 <img className='mt-32 mb-10' src={img1} alt="mobile" />
             </div>
 
-            <div className='bg-blue-900 text-center rounded-xl animate-bounce md:max-w '>
+            <div className='bg-blue-900   rounded-xl animate-bounce md:float-right'>
                 {/* <h1 className='pb-10 text-white float-right text-2xl md:text-3xl '>State of the Art infrastraucure</h1> */}
-                <p className='text-white p-4 rounded-lg'>You can also type a keyword to 
+                <p className='text-white text-center p-4 rounded-lg'>You can also type a keyword to 
                 search online for the video that best fits your document.To make your document look professionally produced, 
                 Word provides header.</p>
             </div>
